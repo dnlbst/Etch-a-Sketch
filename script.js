f2206d84bf8c5bfbe6f50c20771bed75e258a157
Daniel Best <dnelbst@gmail.com>
2020-07-07 13:53:27 +0200

    rest not working

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
-let prompt = 16;
-let gridContainer = document.querySelector('.grid-container');
+let squares = 16; 
+const gridContainer = document.querySelector('.grid-container');
+const prmpt = document.querySelector('.prompt');
 
-
-for (i = 0; i < prompt ** 2; i++) {
-  let gridItem = document.createElement('div');
-  gridItem.classList.add('grid-item');
-  gridContainer.appendChild(gridItem);
+// creating the grid container depending on user input (prompt & button)
+let buildContainer = function(){
+gridContainer.style.cssText = `
+  grid-template-columns:  repeat(${squares}, 1fr);
+  grid-template-rows:     repeat(${squares}, 1fr)
+  `
 };
 
+// filling the grid container with squares
+let fillGrid = function(){
+  for (i = 0; i < squares ** 2; i++) {
+    let gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    gridContainer.appendChild(gridItem);
+  };
+};
+
+// resetting the grid container squares
+let removeGrid = function(){
+  for (i = 0; i < squares ** 2; i++) {
+    gridContainer.removeChild('.gridItem');
+  };
+};
+
+ 
+
+//changing and keeping the color after hovering those damn squares (took me while)
+let changeClr = function(){
 const square = document.querySelectorAll('.grid-item');
-console.log(square);
-  
+
 square.forEach(foreachgriditem =>{
       foreachgriditem.addEventListener('mouseover', e =>{
-      square.classList.add('gird-item--color');
+        foreachgriditem.classList.add('grid-item--color'); // remember! no dot when adding a class!! 
       });
     });
+  };
 
+//function to call all the functions above for grid drawing
+let draw = function(){
+  buildContainer();
+  fillGrid(); 
+  changeClr();
+};
+draw();
 
-// color.foreach(a => {
-//   a.addEventListener('mouseover', e =>{
-//   classList.add(`${'.grid-item--color'}`);
-// });
-// });
+prmpt.addEventListener('click', e =>{
+    removeGrid();
+    prompt = prompt("how many squares per side to make the new grid?", 32);
+    squares = prompt;
+    draw();
+  });
+  
+  
 
-
-
-
-
-
-// const setColor = function('mouseover', e =>{
-//   let gridy = document.getElementsByClassName('grid-item');
-//   gridy.classList.add('gird-item--color');
-// };
-
-
-
-// for (i = 0; i < square.lenght; i++){
-//   square.addEventListener('mouseover', e =>{
-//     square.classList.add('.gird-item--color');
-//   });
-//   };
+  
